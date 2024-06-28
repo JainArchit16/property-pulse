005b5a850e9ba1bdd6e5fef5a018e5e6d743c294
Archit <jainarchit1604@gmail.com>
2024-06-28 23:26:04 +0530

    Added Otp Mailer

--- mail/templates/emailVerificationTemplate.js
+++ mail/templates/emailVerificationTemplate.js
@@ -67,18 +67,18 @@ const otpTemplate = (otp, link) => {
       <body>
           <div class="container">
               <a href=${link}><img class="logo"
-                      src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+                      src="https://i.ibb.co/7Xyj3PC/logo.png" alt="PropertyPulse Logo"></a>
               <div class="message">OTP Verification Email</div>
               <div class="body">
                   <p>Dear User,</p>
-                  <p>Thank you for registering with StudyNotion. To complete your registration, please use the following OTP
+                  <p>Thank you for registering with PropertyPulse. To complete your registration, please use the following OTP
                       (One-Time Password) to verify your account:</p>
                   <h2 class="highlight">${otp}</h2>
                   <p>This OTP is valid for 5 minutes. If you did not request this verification, please disregard this email.
                   Once your account is verified, you will have access to our platform and its features.</p>
               </div>
               <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-                      href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+                      href="mailto:info@propertyPulse.com">info@propertyPulse.com</a>. We are here to help!</div>
           </div>
       </body>
       
